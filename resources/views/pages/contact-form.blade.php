ef67aac762d6963606ee5dfac666544edcfbbfb9
Martin Hlaváč <hlavacm@hotmail.com>
2021-11-27 15:17:56 +0100

    Show contact form errors

--- resources/views/pages/contact-form.blade.php
+++ resources/views/pages/contact-form.blade.php
@@ -1,20 +1,46 @@
+@if ($errors->any())
+    <ul class="alert alert-danger">
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
+
 <form method="POST" action="{{ route('pages.contact') }}">
     @csrf
     <div class="form-group">
         <label for="name-field">{{ __('Name:*') }}</label>
-        <input id="name-field" name="name" type="text" maxlength="100" class="form-control">
+        <input id="name-field" name="name" type="text" class="form-control @error('name') is-invalid @enderror"
+            maxlength="100" value="{{ old('name') }}">
+        @error('name')
+            <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
     </div>
     <div class="form-group">
         <label for="email-field">{{ __('E-mail:*') }}</label>
-        <input id="email-field" name="email" type="email" maxlength="250" class="form-control">
+        <input id="email-field" name="email" type="email" class="form-control @error('email') is-invalid @enderror"
+            maxlength="250" value="{{ old('email') }}">
+        @error('email')
+            <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
     </div>
     <div class="form-group">
         <label for="subject-field">{{ __('Subject:*') }}</label>
-        <input id="subject-field" name="subject" type="text" maxlength="150" class="form-control">
+        <input id="subject-field" name="subject" type="text" class="form-control @error('subject') is-invalid @enderror"
+            maxlength="150" value="{{ old('title') }}">
+        @error('subject')
+            <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
     </div>
     <div class="form-group">
         <label for="message-field">{{ __('Message:*') }}</label>
-        <textarea id="message-field" name="message" rows="3" maxlength="2000" class="form-control"></textarea>
+        <textarea id="message-field" name="message" class="form-control @error('message') is-invalid @enderror" rows="5"
+            maxlength="2000">{{ old('message') }}</textarea>
+        @error('message')
+            <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
     </div>
-    <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+    <div class="text-right">
+        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+    </div>
 </form>
